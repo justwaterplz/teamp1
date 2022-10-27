39ca0b5111ac28ffff39fadc8104a6f59988440c
justwaterplz <102442975+justwaterplz@users.noreply.github.com>
2022-10-27 17:15:12 +0900

    2페이지2
    2페이지2

--- app/src/main/java/com/example/mjucampusguide/page2nd.java
+++ app/src/main/java/com/example/mjucampusguide/page2nd.java
@@ -14,6 +14,77 @@ public class page2nd extends AppCompatActivity {
         setContentView(R.layout.page2nd);
 
         Button prevBtn = (Button)findViewById(R.id.prevBtn);
+
+        //버튼 객체화
+        //print cs atm restaurant RA ETC 6개
+        Button printBtn1 = (Button)findViewById(R.id.printBtn);
+        Button csBtn1 = (Button)findViewById(R.id.csBtn);
+        Button atmBtn1 = (Button)findViewById(R.id.atmBtn);
+        Button restaurantBtn1 = (Button)findViewById(R.id.restaurantBtn);
+        Button RABtn1 = (Button)findViewById(R.id.RABtn);
+        Button ETCBtn1 = (Button)findViewById(R.id.etcBtn);
+
+        //각 버튼마다 클릭리스너 등록
+        //print
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              이전버튼 누르면 전페이지인 초기화면 나옴
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //CS
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              이전버튼 누르면 전페이지인 초기화면 나옴
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //atm
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              이전버튼 누르면 전페이지인 초기화면 나옴
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //restaurant
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              이전버튼 누르면 전페이지인 초기화면 나옴
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //RA
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              이전버튼 누르면 전페이지인 초기화면 나옴
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //ETC
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              이전버튼 누르면 전페이지인 초기화면 나옴
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
         prevBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
